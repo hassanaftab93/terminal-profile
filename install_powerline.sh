88c48df84b4d08a30f22139d7eca0e9b2993ab3f
Hassan Aftab <hassanaftab.pk@gmail.com>
2025-02-19 13:12:12 +0500

    Update script

--- install_powerline.sh
+++ install_powerline.sh
@@ -3,7 +3,7 @@ set -eux pipefail
 
 # Install Powerline for VIM.
 sudo apt install -y python3-pip
-pip3 install --user powerline-status
+sudo pip3 install --user powerline-status
 sudo cp configs/.vimrc ~/.vimrc
 sudo apt install -y fonts-powerline
 
